3568864c00d6ae8619e5c6ab17111b839682c682
Laurence <1936046@cegepmontpetit.ca>
2024-08-22 12:56:45 -0400

    FCT: Exercice Triangles 2

--- code/BonjourKotlin2/src/main/kotlin/Triangle.kt
+++ code/BonjourKotlin2/src/main/kotlin/Triangle.kt
@@ -1,7 +1,7 @@
 package org.lebrun
 
-fun main(){
-    for (i in 1..4) {
+fun triangle(hauteur : Int){
+    for (i in 1..hauteur) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         repeat(i){
@@ -9,4 +9,12 @@ fun main(){
         }
         println()
     }
+    println()
+}
+
+fun main(){
+    triangle(4)
+    triangle(8)
+    triangle(10)
+    triangle(100)
 }
