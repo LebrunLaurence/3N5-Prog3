61b24d39203103bea63d2b8e7539dd3eab875054
Laurence Lebrun <1936047@cegepmontpetit.ca>
2024-08-29 11:09:45 -0400

    FCT Exercice de compréhension sur les types

--- code/S02/Types/src/main/kotlin/Main.kt
+++ code/S02/Types/src/main/kotlin/Main.kt
@@ -13,4 +13,14 @@ fun main() {
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
     }
+
+    val a = 1                                   // type est int
+    val aa = 1L                                 // type est long
+    val aaa = a * 25 + aa * 100                 // type est long
+    val b = 1.0                                 // type est double
+    val c = "1"                                 // type est string
+    val d = true                                // type est bool
+    val laChose = c + b                         // type est char
+    val k = listOf("1", "2", "3")               // type est list<string>
+    val l = mutableListOf(true, false, true)    // type est list<bool>
 }
