53755e86cd2c896d7b97916f10a15fad120680ac
Laurence Lebrun <1936047@cegepmontpetit.ca>
2024-11-12 09:21:16 -0500

    FCT Commencer S11

---
+++ code/S11/TiroirA/app/src/main/java/com/example/tiroira/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.tiroira
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tiroira.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
